5afd57d1ee125f7ac2614d2c9bbabd169277fe37
lidavid662 <146143936+lidavid662@users.noreply.github.com>
2023-11-13 00:42:39 +0000

    done

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -14,6 +14,10 @@ class BoardsController < ApplicationController
 
     @the_board = matching_boards.at(0)
 
+    matching_posts = Post.where({ :board_id => the_id })
+
+    @list_of_posts = matching_posts.order({ :created_at => :desc })
+
     render({ :template => "boards/show" })
   end
 
